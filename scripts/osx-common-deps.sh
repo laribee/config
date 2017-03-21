0d63acf1ef0cf4d8267b08357ef8cb240fa0d42a
David Laribee <laribee@gmail.com>
2017-03-21 18:45:43 -0400

    Update brew before installing deps

--- scripts/osx-common-deps.sh
+++ scripts/osx-common-deps.sh
@@ -2,5 +2,5 @@ if [ ! -f "/usr/local/bin/brew" ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew upgrade
+brew update
 brew install git vim zsh
